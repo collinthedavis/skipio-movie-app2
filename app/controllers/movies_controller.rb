fb9d49d2efbbed4f3a06fabcb9013f059bb87818
Collin <collinthedavis@gmail.com>
2017-03-30 10:57:02 -0600

    Add basic crud functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,31 @@
 class MoviesController < ApplicationController
 	def index
+		@movies = Movie.all
 	end
 
 	def show
+		@movie = Movie.find(params[:id])
 	end
+
+	def new
+		@movie = Movie.new
+	end
+
+	def create
+    @movie = Movie.new(params.require(:movie).permit(:title, :genre, :overview, :actor, :rating))
+
+    respond_to do |format|
+      if @movie.save
+        format.html { redirect_to movies_path, notice: 'Movie was posted' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  	@movie = Movie.find(params[:id])
+  end
+
+
 end
