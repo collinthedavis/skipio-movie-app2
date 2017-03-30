5c00729eebe3d075826219b5d3c3f15139442154
Collin <collinthedavis@gmail.com>
2017-03-30 11:16:09 -0600

    finish CRUD functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,5 +27,27 @@ class MoviesController < ApplicationController
   	@movie = Movie.find(params[:id])
   end
 
+  def update
+    @movie = Movie.find(params[:id])
+
+    respond_to do |format|
+      if @movie.update(params.require(:movie).permit(:title, :genre, :overview, :actor, :rating))
+        format.html { redirect_to movies_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+
+    @movie.destroy
+
+    respond_to do |format|
+      format.html { redirect_to movies_url, notice: 'Movie was removed.' }
+    end
+  end
+
 
 end
